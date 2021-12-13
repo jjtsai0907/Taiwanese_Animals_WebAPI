29ab8a6d8bfc0fa3c58c2a864204e7b4a40bb06c
jia.jiuan.tsai@gmail.com <jia.jiuan.tsai@gmail.com>
2021-12-13 13:15:08 +0100

    Setup Controller & able to get values on Postman

---
+++ Taiwanese_Animals_WebAPI/Controllers/AnimalController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taiwanese_Animals_WebAPI.Entities;
+using Taiwanese_Animals_WebAPI.Repo;
+
+namespace Taiwanese_Animals_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/taiwanese_animal")]
+    public class AnimalController : ControllerBase
+    {
+        private readonly IAnimalRepo _repo;
+
+        public AnimalController(IAnimalRepo repo)
+        {
+            _repo = repo;
+        }
+
+
+        // GET /api/taiwanese_animal
+        [HttpGet("")]
+        public List<Animal> GetAnimals()
+        {
+            List<Animal> animals = _repo.GetAll();
+            return animals;
+        }
+
+        // GET /api/taiwanese_animal/:id
+        [HttpGet("{id}")]
+        public Animal GetAnimalByID(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+
+
+    }
+}
