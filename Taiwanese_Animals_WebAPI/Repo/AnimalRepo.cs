d95615fe364aff9babeb53de6c2ce4bc5023681c
jia.jiuan.tsai@gmail.com <jia.jiuan.tsai@gmail.com>
2021-12-13 12:44:57 +0100

    Setup IAnimalRepo and AnimalRepo & Create mock data

---
+++ Taiwanese_Animals_WebAPI/Repo/AnimalRepo.cs
@@ -0,0 +1,52 @@
+﻿using Taiwanese_Animals_WebAPI.Entities;
+
+namespace Taiwanese_Animals_WebAPI.Repo
+{
+    public class AnimalRepo : IAnimalRepo
+    {
+
+        private List<Animal> _animals;
+
+        public AnimalRepo()
+        {
+            _animals = PopulateAnimalData();
+        }
+
+
+        public List<Animal> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Animal GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<Animal> PopulateAnimalData()
+        {
+            return new List<Animal>
+            {
+                new Animal {
+                    Id = 1,
+                    EnglishName = "1",
+                    LatinName = "",
+                    Type = ""
+                },
+                new Animal {
+                    Id = 2,
+                    EnglishName = "2",
+                    LatinName = "",
+                    Type = ""
+                },
+                new Animal {
+                    Id = 3,
+                    EnglishName = "3",
+                    LatinName = "",
+                    Type = ""
+                },
+            };
+        }
+
+    }
+}
---
+++ Taiwanese_Animals_WebAPI/Repo/IAnimalRepo.cs
@@ -0,0 +1,14 @@
+﻿using Taiwanese_Animals_WebAPI.Entities;
+
+namespace Taiwanese_Animals_WebAPI.Repo
+{
+    public interface IAnimalRepo
+    {
+        List<Animal> GetAll();
+
+        Animal GetById(int id);
+
+        
+
+    }
+}
